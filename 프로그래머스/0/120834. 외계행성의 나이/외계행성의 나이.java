592296d55bcf7e6d9fc4d8adbc84c1d0655378af
김 다현 <70506979+dhyun10@users.noreply.github.com>
2024-03-04 19:11:00 +0900

    [level 0] Title: 외계행성의 나이, Time: 9.44 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,10 +1,13 @@
 class Solution {
     public String solution(int age) {
         String answer = "";
-        String[] str = String.valueOf(age).split("");
-        for(String a : str) {
-            answer += (char)(Integer.parseInt(a)+'a');
+
+        while(age > 0) {
+            int num = age % 10;
+            answer = ((char)(num+'a')) + answer;
+            age /= 10;
         }
+
         return answer;
     }
 }
