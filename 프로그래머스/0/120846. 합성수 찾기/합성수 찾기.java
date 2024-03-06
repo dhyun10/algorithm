7d057f5611e924ac903b83791baba904fe661ffd
김 다현 <70506979+dhyun10@users.noreply.github.com>
2024-03-06 18:44:37 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        for(int j = 1; j <= n; j++) {
+        for(int j = 2; j <= n; j++) {
             int d = 0;
-            for(int i = 1; i <= n; i++) {
+            for(int i = 1; i <= j; i++) {
                 if(j % i == 0) d++;
             }
             if(d >= 3) answer++;
