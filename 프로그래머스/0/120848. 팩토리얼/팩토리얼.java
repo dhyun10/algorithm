b7c6d0a4ffcbd23d04b3eaf5ffd983d52b3a5fea
김 다현 <70506979+dhyun10@users.noreply.github.com>
2024-03-07 22:04:38 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num = 1;
+        int a = 1;
+        while(true) {
+            num *= a;
+            if(num > n) {
+                answer = a;
+                break;
+            }
+            a++;
+        }
+        return answer-1;
+    }
+}
