7204d7c84d48ada8d5946912495a4db04474969b
김 다현 <70506979+dhyun10@users.noreply.github.com>
2024-03-08 19:39:09 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.93 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(String a : my_string.split("")) {
+            if(!answer.contains(a)) {
+                answer += a;
+            }
+        }
+        return answer;
+    }
+}
